eb93ac0b54e933c7256e5bcd09f5850ea325e17a
ALZSM_wei <2713517764@qq.com>
2023-03-09 08:31:14 +0800

    3月9号

--- test2_27.c
+++ test2_27.c
@@ -473,15 +473,32 @@
 //}
 
 // 实现memory move函数的功能
-#include <stdio.h>
-#include <memory.h>
-int main(void)
-{
-    int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
-    memmove(arr, arr + 4, 5*4);
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d\n", arr[i]);
-    }
-    return 0;
-}
+//#include <stdio.h>
+//#include <memory.h>
+//int main(void)
+//{
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//    memmove(arr, arr + 4, 5*4);
+//    for (int i = 0; i < 10; i++)
+//    {
+//        printf("%d\n", arr[i]);
+//    }
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//    typedef struct book
+//    {
+//        char name[20];
+//        int price;
+//    }books;
+//
+//    books a = { "hello", 20 };
+//    printf("%s\n", a.name);
+//    printf("%d\n", a.price);
+//
+//    return 0;
+//}
+
